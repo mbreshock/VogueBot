302b8adb744ed4a21b9661ff596ca858c748d845
Michael Breshock <mbreshock@gmail.com>
2024-12-14 15:46:27 -0800

    got flask to work with multiple images

--- app.py
+++ app.py
@@ -4,15 +4,17 @@ import shaped
 
 app = Flask(__name__)
 
+DEFAULT_ID_CODE = "783346030"  # Default ID code to pre-fill in the input box
+API_KEY = "dQRowDdtmU4mCZdHyKALGGvYJxOo9Mw9Ye5bR544" # Yujian's Shaped.AI API Key
+
 # Route for the home page
 @app.route("/", methods=["GET", "POST"])
 def index():
-    image_url = None
+    products = []
     error_message = None
-    API_KEY = "dQRowDdtmU4mCZdHyKALGGvYJxOo9Mw9Ye5bR544"
 
     if request.method == "POST":
-        id_code = request.form.get("id_code")  # Get the ID code from the form
+        id_code = request.form.get("id_code", DEFAULT_ID_CODE)  # Get the ID code from the form or use default
         if not id_code:
             error_message = "Please enter an ID code."
         else:
@@ -40,13 +42,26 @@ def index():
                         limit=5,
                     ),
                 )
+                # convert response to dictionary
                 resp_dict = response.to_dict()
-                image_url = resp_dict['metadata'][1]['image']
+                # get metadata
+                meta = resp_dict["metadata"]
+                # for each of the 5 responses, create a dictionary of product name, product id, and image url
+                for i in range(0, 5):
+                    prod_dict = {"product_id": meta[i]['item_id'], 
+                                 "product_name": meta[i]['product_name'], 
+                                 "image_url": meta[i]['image']}
+                    products.append(prod_dict)
 
             except Exception as e:
                 error_message = f"Failed to fetch image: {str(e)}"
 
-    return render_template("index.html", image_url=image_url, error_message=error_message)
+    return render_template(
+        "index.html", 
+        default_id_code=DEFAULT_ID_CODE, 
+        products=products, 
+        error_message=error_message
+    )
 
 
 if __name__ == "__main__":
